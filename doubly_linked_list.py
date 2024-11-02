00d9618959c16a5809933bd0b2208b94652e1dfa
ItUser-Softwares <137013012+ItUser-Softwares@users.noreply.github.com>
2024-11-02 12:44:13 +0500

    First Commit

---
+++ doubly_linked_list.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+    
+node1 = Node(3)
+node2 = Node(5)
+node3 = Node(13)
+node4 = Node(2)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+node2.previous = node1
+node3.previous = node2
+node4.previous = node3
+
+
+print("\nTraversing forward:")
+currentNode = node1
+while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.next
+print("null")
+
+print("\nTraversing backward:")
+currentNode = node4
+while currentNode:
+    print(currentNode.data, end=" -> ")
+    currentNode = currentNode.previous
+print("null")
